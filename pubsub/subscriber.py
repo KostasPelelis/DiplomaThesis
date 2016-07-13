71581fe03763f10e483cd39576192f7c65fb6d84
Kostas Pelelis <kpelelisos@gmail.com>
2016-07-13 19:01:16 +0300

    Pretified the log format

--- pubsub/subscriber.py
+++ pubsub/subscriber.py
@@ -4,12 +4,14 @@ import os
 import sys
 import threading
 import json
-import logger
 import time
 import redis
 import collections
+import logging
 
-log = logger.init_logging()
+from logger import Style
+log = logging.getLogger('noc-netmode')
+
 blockedIPs = collections.OrderedDict()
 
 class TimerThread(threading.Thread):
@@ -31,10 +33,19 @@ class TimerThread(threading.Thread):
                     printflush(announcement)
                     blockedIPs.pop(key, None)
             if self.debug:
-                if not blockedIPs:
-                    log.info("IP\t\t: Duration")
-                for k,v in blockedIPs.items():
-                    log.info("{0}\t: {1}".format(k,v))
+                if blockedIPs:
+                    log.info("|--------------TIMEOUT TABLE---------------|")
+                    log.info("|{0}{2}{1}| {0}{3}{1}|".format(
+                        Style.BOLD,
+                        Style.ESCAPE,
+                        "IP".ljust(20),
+                        "Duration".ljust(20) 
+                    ))
+                    log.info("|------------------------------------------|")
+                    for k,v in blockedIPs.items():
+                        log.info("|{0}| {1}|".format(k.ljust(20),str(v).ljust(20)))
+                    log.info("|------------------------------------------|")
+
             time.sleep(self.tick)
 
 class SubThread(threading.Thread):
@@ -57,10 +68,10 @@ class SubThread(threading.Thread):
                 printflush (e)
                 continue
             event = jsondata['event']
-            log.info("New message received -------------------")
+            log.info(Style.BOLD + "--------------------- New message received ---------------------" + Style.ESCAPE)
             for (key,value) in sorted(event.items()):
-                log.info("{0}\t\t: {1}".format(key,value))
-            log.info("End of message--------------------------")
+                log.info("{0}: {1}".format(key.ljust(20),value))
+            log.info(Style.BOLD + "--------------------- End of message ---------------------" + Style.ESCAPE)
 
             # Checking exclusions. If none matches then we proceed with the announcement 
             if event['alert']['severity'] > self.args['severity_thres']:
@@ -76,13 +87,13 @@ class SubThread(threading.Thread):
             elif event['src_port'] in self.args['exclude_src_port']:
                 log.info("Event excluded due to src port number")
             else:
-                log.info("New IP is about to be announced------------------------------")
-                log.info("Source\t\t: {}:{}".format(event['src_ip'], event['src_port']))                      
-                log.info("Destination\t: {}:{}".format(event['dest_ip'],event['dest_port']))
-                log.info("Category\t\t: {}".format(event['alert']['category']))
-                log.info("Signature_ID\t: {}".format(event['alert']['signature_id']))                      
-                log.info("Signature\t\t: {}".format(event['alert']['signature']))                      
-                log.info("Announcement is logged and about to be sent to exabgp--------")
+                log.info(Style.BOLD + "--------------------- New IP Announcement ---------------------" + Style.ESCAPE)
+                log.info("|{0} |{1}|".format("Source".ljust(20), (str(event['src_ip']) + ':' + str(event['src_port'])).ljust(100)))
+                log.info("|{0} |{1}|".format("Destination".ljust(20), (str(event['dest_ip']) + ':' + str(event['dest_port'])).ljust(100)))
+                log.info("|{0} |{1}|".format("Category".ljust(20), str(event['alert']['category']).ljust(100)))
+                log.info("|{0} |{1}|".format("Signature ID".ljust(20), str(event['alert']['signature_id']).ljust(100)))
+                log.info("|{0} |{1}|".format("Announcement".ljust(20), str(event['alert']['signature']).ljust(100)))
+                log.info(Style.BOLD + "--------------------- Annoncement is logged, sending to exabgp ---------------------" + Style.ESCAPE)
                 announcement = "announce route {}/32 next-hop {}".format(event['src_ip'], self.args['dest'])
                 if event['src_ip'] not in blockedIPs:
                     log.warning(announcement)
@@ -113,35 +124,33 @@ def setupenvironment(args):
         printflush ("Please supply the redis server with the --redis_host option", file=sys.stderr)
         sys.exit(1)
 
-if __name__ == '__main__':
+""" Parsing arguments, checking the input values
+    and setting up the environment
+"""
+parser = argparse.ArgumentParser(description='Subscriber parser')
+parser.add_argument('--debug', '-d', help='Run in debug mode', default=False, action='store_true')
+# TODO: add regex for IPv4
+parser.add_argument('--redis_host', '-r', help='Connect to redis server and subscribe to an event channel')
+parser.add_argument('--redis_port', '-p', help='Redis server port.The default value is 6379',type=int, default=6379)
+parser.add_argument('--user', '-u', help='Drop privileges to supplied userid.The default user privileges is nobody\'s')
+parser.add_argument('--duration', help='Duration of the BGP announcement. After that the announcement is withdrawn',type=int, default=60)
+parser.add_argument('--tick', '-t', help='How often we will update the TTLs of BGP announcements',type=int, default=10)
+parser.add_argument('--dest', help='Announce the next hop to be the supplied value.Default value \'self\'', default='self')
+parser.add_argument('--severity-thres', help='Alerts with severity higher than the given threshold will be ignored.Defaults to 1.',type=int, default=1)
+parser.add_argument('--exclude-sig', help='Exclude alerts with the given signature_id from processing.This can be used multiple times',type=int, nargs='*',default=[])
+parser.add_argument('--exclude-source', help='Exclude alerts with given source IP CIDR.This can also be used multiple times', nargs='*', default=[])
+parser.add_argument('--exclude-destination', help='Exclude alerts with given destination IP CIDR.This can also be used multiple times', nargs='*', default=[])
+parser.add_argument('--exclude-dest-port', help='Exclude alerts with given destination port number.This can be used multiple times',type=int, nargs='*', default=[])
+parser.add_argument('--exclude-src-port', help='Exclude alerts with given source port number.This can be used multiple times',type=int, nargs='*', default=[])
+parser.add_argument('--channel', '-c', help='Name of channel to subscribe, defaults to \'suricata\' ', default='suricata')
+args = vars(parser.parse_args())
+setupenvironment(args)
 
-    """ Parsing arguments, checking the input values
-        and setting up the environment
-    """
-    parser = argparse.ArgumentParser(description='Subscriber parser')
-    parser.add_argument('--debug', '-d', help='Run in debug mode', default=False, action='store_true')
-    # TODO: add regex for IPv4
-    parser.add_argument('--redis_host', '-r', help='Connect to redis server and subscribe to an event channel')
-    parser.add_argument('--redis_port', '-p', help='Redis server port.The default value is 6379',type=int, default=6379)
-    parser.add_argument('--user', '-u', help='Drop privileges to supplied userid.The default user privileges is nobody\'s')
-    parser.add_argument('--duration', help='Duration of the BGP announcement. After that the announcement is withdrawn',type=int, default=60)
-    parser.add_argument('--tick', '-t', help='How often we will update the TTLs of BGP announcements',type=int, default=10)
-    parser.add_argument('--dest', help='Announce the next hop to be the supplied value.Default value \'self\'', default='self')
-    parser.add_argument('--severity-thres', help='Alerts with severity higher than the given threshold will be ignored.Defaults to 1.',type=int, default=1)
-    parser.add_argument('--exclude-sig', help='Exclude alerts with the given signature_id from processing.This can be used multiple times',type=int, nargs='*',default=[])
-    parser.add_argument('--exclude-source', help='Exclude alerts with given source IP CIDR.This can also be used multiple times', nargs='*', default=[])
-    parser.add_argument('--exclude-destination', help='Exclude alerts with given destination IP CIDR.This can also be used multiple times', nargs='*', default=[])
-    parser.add_argument('--exclude-dest-port', help='Exclude alerts with given destination port number.This can be used multiple times',type=int, nargs='*', default=[])
-    parser.add_argument('--exclude-src-port', help='Exclude alerts with given source port number.This can be used multiple times',type=int, nargs='*', default=[])
-    parser.add_argument('--channel', '-c', help='Name of channel to subscribe, defaults to \'suricata\' ', default='suricata')
-    args = vars(parser.parse_args())
-    setupenvironment(args)
-
-    # Initializing Redis and the appropriate threads
-    r = redis.StrictRedis(host=args['redis_host'], port=args['redis_port'], db=0)
-    timerThread = TimerThread(args['debug'], args['tick'], args['dest'])
-    pubsubThread = SubThread(r, args)
-    timerThread.start()
-    pubsubThread.start()
-    timerThread.join()
-    pubsubThread.join()
+# Initializing Redis and the appropriate threads
+r = redis.StrictRedis(host=args['redis_host'], port=args['redis_port'], db=0)
+timerThread = TimerThread(args['debug'], args['tick'], args['dest'])
+pubsubThread = SubThread(r, args)
+timerThread.start()
+pubsubThread.start()
+timerThread.join()
+pubsubThread.join()
