5507a875a35293b17950a8d03ceeb069d785d655
Kostas Pelelis <kpelelisos@gmail.com>
2016-07-13 19:01:54 +0300

    Main can run both publisher and subscriber

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
 from logger import init_logging
-log = init_logging('noc-netmode')
+init_logging('noc-netmode')
 
-from tests.test_pub import TestPublisher
-import unittest
-log = init_logging('noc-netmode')
 from pubsub import subscriber
-
-if __name__ == "__main__":
-	unittest.main()
