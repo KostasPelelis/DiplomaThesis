1c0fdc97d3f370bb8862c97115b35c5ab0475238
Kostas Pelelis <kpelelisos@gmail.com>
2016-08-24 19:50:28 +0300

    added the option to add parenthesis to variables

--- policy_engine/util.py
+++ policy_engine/util.py
@@ -2,7 +2,7 @@ def format_args(args):
 	ret = []
 	for arg in args:
 		if isinstance(arg, str) and arg[0] == '$':
-			ret.append({'type': 'ref', 'value': arg[1:]})
+			ret.append({'type': 'ref', 'value': arg[2:-1] if arg[1] == '(' else arg[1:]})
 		else:
 			ret.append({'type': 'val', 'value': arg})
 	return ret
@@ -11,7 +11,8 @@ def format_kwargs(kwargs):
 	ret = {}
 	for arg_key, arg_value in kwargs.items():
 		if isinstance(arg_value, str) and arg_value[0] == '$':
-			ret[arg_key] = {'type': 'ref', 'value': arg_value[1:]}
+			ret[arg_key] = {'type': 'ref', 'value': arg_value[2:-1] if arg_value[1] == '(' else arg_value[1:]}
+			print(ret[arg_key])
 		else:
 			ret[arg_key] = {'type': 'val', 'value': arg_value}
 	return ret
