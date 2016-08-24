a6d3ae5db09e05f9b0a2e402d218dfdde6291463
Kostas Pelelis <kpelelisos@gmail.com>
2016-08-24 19:30:33 +0300

    Added a custom dict type to get data from nested dictionaries'

--- policy_engine/policy.py
+++ policy_engine/policy.py
@@ -7,6 +7,25 @@ class Policy:
 	The base Policy class that will check the conditions and will do an action
 	according to the schema file
 	"""
+class EventData(object):
+
+	def __init__(self, data):
+		self.data = data
+
+	def __getitem__(self, key):
+		val = self.data
+		for fragment in key.split('.'):
+			if isinstance(val, dict) and fragment not in val.keys():
+				raise KeyError
+			val = val[fragment]
+		return val
+
+	def __setitem__(self, key, val):
+		_dict = self.data
+		fragments = key.split('.')
+		for fragment in fragments[:-1]:
+			_dict = _dict[fragment]
+		_dict[fragments[-1]] = val
 
 	def __init__(self, event=None, name=None, conditions=None, action=None):
 
@@ -22,7 +41,7 @@ class Policy:
 		self.action	= Action(name=action["name"], args=action["arguments"])
 
 	def trigger(self, event_data):
-
+		event_data = EventData(event_data)
 		if self._validate_conditions(event_data):
 			self.action.run(event_data)
 
